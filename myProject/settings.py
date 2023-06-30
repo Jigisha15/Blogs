f0f2c702d3a53c34a55a7e2f786a24fb1e4c99da
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-06-30 21:25:55 +0530

    passwords

--- myProject/settings.py
+++ myProject/settings.py
@@ -126,3 +126,8 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# PASSWORDS OF REGISTERED USERS
+# Superuser: id-admin, password-admin
